47ea751a4d8419e5624b3bc6627688eb0517ab64
Alpyg <alpyg@pm.me>
2025-04-06 14:31:53 -0400

    Updated zig version

--- flake.nix
+++ flake.nix
@@ -1,13 +1,30 @@
 {
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = inputs@{ self, nixpkgs, ... }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs;
-          [
+  description = "An empty project that uses Zig.";
 
-          ];
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig.url = "github:Fryuni/zig-overlay";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
     };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    let
+      overlays =
+        [ (final: prev: { zigpkgs = inputs.zig.packages.${prev.system}; }) ];
+
+      systems = builtins.attrNames inputs.zig.packages;
+    in flake-utils.lib.eachSystem systems (system:
+      let pkgs = import nixpkgs { inherit overlays system; };
+      in rec {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ zigpkgs.master zigpkgs.master.zls ];
+        };
+
+        devShell = self.devShells.${system}.default;
+      });
 }
